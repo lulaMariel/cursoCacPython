076eeda3395a6a5ddcbb1c88b331dc7cabbc2a70
lulaMariel <luciana.lavalle@gmail.com>
2024-09-30 15:18:30 -0300

    Hago practica1

---
+++ practicasPython/practicasClase3/clase/practica1.py
@@ -0,0 +1,21 @@
+# Variables
+
+cadena = "123" #str
+entero = 45 # int
+flotante = 45.5 # float
+
+# Conversión de datos
+
+int(cadena)
+str(entero)
+int(flotante)
+
+# Conversión con info
+
+nombre = "Lula"
+edad = 21
+
+print()
+print(type(nombre))
+print(type(edad))
+print(type(flotante))
